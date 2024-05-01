934a25ccbaa0273f7817405d8b13cd0bce21e9dd
Benjamin Kemp <bjkemp@gmail.com>
2024-05-01 11:28:01 -0400

    Refactored ps-token

--- powerschool-token.js
+++ powerschool-token.js
@@ -1,138 +1,107 @@
-const axios = require( 'axios' );
-const https = require( 'https' );
+const axios = require('axios');
+const https = require('https');
 
-var _internals = {};
+const BASE_URL = 'https://'; // Avoid string concatenation for the base URL
 
-_internals.getToken = function (props, cb) {
-    const instance = axios.create({
-        httpsAgent: new https.Agent({  
-            rejectUnauthorized: props.ssl_reject
-        })
-    });
-    
-    const ps_hash = (new Buffer.from(
-        props.client
-        + ":" 
-        + props.secret
-    )).toString('base64');
+function createAxiosInstance(props) {
+  return axios.create({
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: props.ssl_reject,
+    }),
+  });
+}
 
-    instance.post(
-        'https://' + props.host + '/oauth/access_token',
-        "grant_type=client_credentials",
-        {
-            headers: {
-                "Authorization": "Basic " + ps_hash,
-								"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-            }
+function getPsHash(client, secret) {
+  return (new Buffer.from(`${client}:${secret}`)).toString('base64');
+}
+
+async function getAccessToken(props) {
+  const instance = createAxiosInstance(props);
+  const psHash = getPsHash(props.client, props.secret);
+
+  try {
+    const response = await instance.post(
+      `${BASE_URL}${props.host}/oauth/access_token`,
+      "grant_type=client_credentials",
+      {
+        headers: {
+          Authorization: `Basic ${psHash}`,
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error retrieving access token: ${error}`);
+  }
+}
+
+function getNodeProperties(node, context, propertyType) {
+  const globalContext = context.global;
+  const flowContext = context.flow;
+
+  // Prioritize msg context first, then flow, then global
+  return node[propertyType] ?? flowContext.get(node[propertyType]) ?? globalContext.get(node[propertyType]);
+}
+
+function isTokenValid(psApi) {
+  if (!psApi || !psApi.expires_in) {
+    return false;
+  }
+  const now = new Date();
+  const expiry = new Date(psApi.expires);
+  return expiry > now && (expiry.getTime() - now.getTime()) / 1000 >= 120; // Account for 2-minute buffer
+}
+
+module.exports = function (RED) {
+  RED.nodes.createNode(this, function (n) {
+    const node = this;
+
+    // Retrieve properties using helper function
+    const props = {
+      client: getNodeProperties(n, this.context, 'client'),
+      secret: getNodeProperties(n, this.context, 'secret'),
+      host: getNodeProperties(n, this.context, 'host'),
+      ssl_reject: n.ssl_reject === 'true',
+    };
+
+    this.on('input', async function (msg) {
+      const datetime = new Date().toLocaleString();
+      node.status({ fill: 'blue', shape: 'dot', text: datetime });
+
+      let psApi = this.context().global.get('ps_api');
+      let getPsToken = true;
+
+      if (psApi) {
+        getPsToken = !isTokenValid(psApi);
+      }
+
+      if (getPsToken) {
+        node.warn('Generating New PS API Token');
+        try {
+          const token = await getAccessToken(props);
+
+          if (!msg.hasOwnProperty('ps_token')) {
+            msg.ps_token = {};
+          }
+
+          msg.ps_token = token;
+          msg.ps_token.host = props.host;
+          msg.ps_token.ssl_reject = props.ssl_reject;
+          msg.ps_token.expires = new Date();
+          msg.ps_token.expires.setSeconds(msg.ps_token.expires.getSeconds() + token.expires_in);
+          node.warn(`Token Expires: ${msg.ps_token.expires}`);
+
+          this.context().global.set('ps_api', msg.ps_token);
+          node.send(msg);
+        } catch (error) {
+          node.error(error.message);
         }
-
-    ).then(response => {
-      cb(response.data, null);
-    }).catch(error => {
-      cb(null, {error: error, props: props});
+      } else {
+        psApi.expires_in -= 120; // Update remaining time
+        this.context().global.set('ps_api', psApi);
+        node.send(msg);
+      }
     });
-};
-
-module.exports = function(RED) {
-	'use strict';
-
-	function Node(n) {
-	  
-		RED.nodes.createNode(this,n);
-
-		const node = this
-		const globalContext = this.context().global
-		const flowContext = this.context().flow
-		
-		this.on('input', function (msg) {
-			const datetime = new Date().toLocaleString()
-			node.status({ fill: 'blue', shape: 'dot', text: datetime })
-
-			// Retrieve the node's properties
-			const props = {
-				client: n.client ?? process.env.POWERSCHOOL_CLIENT_ID,
-				secret: n.secret ?? process.env.POWERSCHOOL_CLIENT_SECRET,
-				host: n.host ?? process.env.POWERSCHOOL_HOST,
-				ssl_reject: (n.ssl_reject == 'true')
-			}
-
-			if ( n.clientType === 'global' ) {
-				props.client = globalContext.get(n.client)
-			} else if ( n.clientType === 'flow' ) {
-				props.client = flowContext.get(n.client)
-			} else if ( n.clientType === 'msg' ) {
-				props.client = msg[n.client]
-			}
-
-			if ( n.secretType === 'global' ) {
-				props.secret = globalContext.get(n.secret)
-			} else if ( n.secretType === 'flow' ) {
-				props.secret = flowContext.get(n.secret)
-			} else if ( n.secretType === 'msg' ) {
-				props.secret = msg[n.secret]
-			}
-
-			if ( n.hostType === 'global' ) {
-				props.host = globalContext.get(n.host)
-			} else if ( n.hostType === 'flow' ) {
-				props.host = flowContext.get(n.host)
-			} else if ( n.hostType === 'msg' ) {
-				props.host = msg[n.host]
-			}
-
-			var ps_api = globalContext.get( 'ps_api' );
-			var get_ps_token = true;
-
-			if ( ps_api ) {
-				if ( ps_api.hasOwnProperty( 'expires_in' ) ) {
-					if ( ps_api.expires_in >= 120 ) { 
-						get_ps_token = false;
-					}
-				}
-			}
-
-			if ( get_ps_token == true ) {
-				node.warn( 'Generating New PS API Token' );
-
-				_internals.getToken( props, function(result, error){
-					// if ( error?.error ) {
-					// 	node.status({ fill: 'red', shape: 'dot', text: JSON.stringify(error) })
-					// 	node.error( JSON.stringify(error) );
-					// 	return;
-					// }
-
-					// node.error(`Result: ${JSON.stringify(result)}`)
-
-					if (!msg.hasOwnProperty('ps_token')) {
-						msg.ps_token = {};
-					}
-
-					if ( msg.hasOwnProperty('ps_token') ) {
-						msg.ps_token = result
-						msg.ps_token.host = props.host
-						msg.ps_token.ssl_reject = props.ssl_reject
-
-						msg.ps_token.expires = new Date()
-						msg.ps_token.expires.setSeconds( msg.ps_token.expires.getSeconds() + result.data.expires_in )
-						node.warn( 'Token Expires: ' + msg.ps_token.expires )
-						globalContext.set( 'ps_api', msg.ps_token )
-						node.send(msg);
-					} else {
-						node.error( JSON.stringify(result) );
-					}
-				});
-
-			} else {
-
-				ps_api.expires_in -= 120;
-				globalContext.set( 'ps_api', ps_api );
-
-				node.send( msg );
-
-
-			}
-		});
-	}
-
-	RED.nodes.registerType('powerschool-token', Node);
+  });
 };
